a063d0b7dc3d7ba932a20c587ee93d7c5299b4b3
Dorn Moore <dorn@savingcranes.org>
2025-01-23 15:47:56 -0600

    Example secret_stuff.py file
    This is where I store my secret stuff. make sure you add secret_stff.py to your git.ignore so you don't share your logins

---
+++ secret_stuff example.py
@@ -0,0 +1,11 @@
+AGOportal = "https://savingcranes.maps.arcgis.com"
+AGOuser = "your_username"
+AGOpass = "your_password"
+
+# connect to the database
+db_host = 'your_db_host'
+db_name = 'your_db_name'
+db_port = 'your_db_port'
+db_user = 'your_db_user'
+db_pass = 'your_db_password'
+db_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
